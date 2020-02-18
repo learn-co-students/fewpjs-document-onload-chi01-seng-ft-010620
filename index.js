4edea9b77a6bbf0bd8a33874e1d76e872f205100
m-gizzi <matthewgizzi@verizon.net>
2020-02-18 16:12:20 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let change = document.getElementById('text')
+    change.innerHTML = "This is really cool!"
+  });
