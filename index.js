ec126c9336c2702a355b40d743c369d9651a77fc
github username <github email address>
2020-02-18 21:11:36 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   let text = document.getElementById('text');
+  let x = text.innerHTML = "This is really cool!";
+ 
+});
+
+
+
